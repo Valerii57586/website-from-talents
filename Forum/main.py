07caa1bc6eed02ff47faa1a15b98e8722eae67d5
Sergey Danielan <130085050+1dff1@users.noreply.github.com>
2025-02-12 18:09:35 +0300

    login created

--- Forum/main.py
+++ Forum/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 from sqltools import sqltools as sq
 
 
@@ -17,13 +17,26 @@ def register():
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
+        if sq.exists_in_table(table_name="data", condition=("email", email), dbname="users.db"):
+            return "Такой email существует"
         try:
             sq.add_record(table_name="data", values={"username": username, "password": password, "email": email}, dbname="users.db")
-            return "It works"
+            return redirect(url_for("login"))
         except:
             return "Error"
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        if sq.get_column_value_by_name(table_name="data", column_to_get="password", condition=("email", email), dbname="users.db") == password:
+            return redirect(url_for("main"))
+        else:
+            return redirect(url_for("register"))
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
